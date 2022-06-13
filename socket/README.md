bcb9a56580f312f7e00b21f151cf6d48d99eb9a4
Håkon Bårsaune <haakonbaa@gmail.com>
2022-06-13 20:02:33 +0200

    action class and more text parsing

---
+++ socket/README.md
@@ -0,0 +1,8 @@
+# Socket
+
+responsible for reading file and communicating with client when terminal is updated
+
+## Supported ANSI-escape-codes:
+|Code|Name|Effect|
+|-|-|-|
+|CSI n A|Cursor up||
--- socket/screen.py
+++ socket/screen.py
@@ -1,26 +1,44 @@
 from curses import raw
+from dataclasses import dataclass
 import re
 
 ANSIESC = re.compile(r'\x1b\[([0-9;\?]*[a-zA-Z]{1})')
+@dataclass
+class Action:
+    action : str
+
 
 class Terminal:
-    def __init__(self, rows : int = 24, cols : int = 24):
+    def __init__(self, rows: int = 24, cols: int = 24, self.):
         self.rows = rows
         self.cols = cols
         self.cursorR = 0
         self.cursorC = 0
-    
+
+    def print_raw():
+        pass
+
     def print(self, text):
         index = 0
-        rawText = []
-        for m in ANSIESC.finditer(text):
+        textAction = []
+        escCodes = [*ANSIESC.finditer(text)]
+        for m in escCodes:
             if not m.start() == index:
-                rawText.append(text[index:m.start()])
+                textAction.append(text[index:m.start()])
+            textAction.append(Action(m.string[m.start()+2:m.end()]))
             index = m.end()
         if not index == len(text):
-            rawText.append(text[index:len(rawText)])
-        print(rawText)
-        
+            textAction.append(text[index:])
+        for tora in textAction:
+            if isinstance(tora,Action):
+                self.do_action(tora)
+            else:
+                self.print_raw(tora)
+
+    
+    def do_action(self, action : str):
+        # :TODO action
+        print(action)
 
 t = Terminal()
 t.print('\x1b[31mDette er en \x1b[32mtest\x1b[0m')
